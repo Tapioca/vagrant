3a22c320dddb71c33ab3a139d11fa9b0ac36c781
Michael Lefebvre <michael@scenedata.com>
2015-10-27 21:34:16 +0100

    install script enhancement

--- provision/install.sh
+++ provision/install.sh
@@ -19,13 +19,26 @@ apt-get update -y
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
-# nginx git mongodb php mysql
+# nginx git mongodb php mysql node npm
 apt-get install -y --force-yes  nginx git mongodb-org redis-server build-essential openssl libssl-dev pkg-config php5-fpm php5-gd php5-common php5-curl php5-mcrypt php5-memcache php5-mysql php5-intl php5-cli php5-mongo php5-redis php-apc mysql-server mysql-client-core-5.5 mysql-client-5.5
+
+# bonus
+# nodejs npm
+
+# install gulp
+#npm install -g gulp
 
 # Start mongod
 service mongod start
 
-# Composer
+# Add mcrypt
+php5enmod mcrypt
+/etc/init.d/php5-fpm restart
+
+# create mysql DB
+mysql -uroot -proot -e "create database tapioca_dev"
+
+# global PHPunit install
 wget https://phar.phpunit.de/phpunit.phar
 chmod +x phpunit.phar
 mv phpunit.phar /usr/bin/phpunit
@@ -33,17 +46,13 @@ mv phpunit.phar /usr/bin/phpunit
 php -r "readfile('https://getcomposer.org/installer');" | php
 mv composer.phar /usr/bin/composer
 
-# Add mcrypt
-php5enmod mcrypt
-/etc/init.d/php5-fpm restart
+# laravel bins
+composer global require "laravel/installer=~1.1"
 
-# create DB
-mysql -uroot -proot -e "create database tapioca_dev"
+# Add laravel Composer Bin to global path
+echo 'export PATH="$PATH:~/.composer/vendor/bin"' >> ~/.bashrc
+source ~/.bashrc
 
 # bootstrap app
 cd /var/www/tapioca.dev/
 composer install -n
-php artisan key:generate
-# php artisan migrate --package=cartalyst/sentry
-# php artisan migrate
-# php artisan db:seed
